e2ddd62aa870c1378e507d36e8105e4d09116d03
JunXin02 <95616470+JunXin02@users.noreply.github.com>
2026-06-24 17:21:12 +0600

    Update hls.js

--- api/hls.js
+++ api/hls.js
@@ -3,8 +3,8 @@ export const config = {
 };
 
 const UPSTREAM_HEADERS = {
-  Referer: "https://www.livetvpro.site/",
-  Origin: "https://www.livetvpro.site",
+  Referer: "https://live.tsports.com/",
+  Origin: "https://live.tsports.com",
   "User-Agent":
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:151.0) Gecko/20100101 Firefox/151.0",
   Accept: "*/*",
