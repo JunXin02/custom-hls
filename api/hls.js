10b0e5fb240c8bdfe7a7e55d6cb67e0590c0a9e3
JunXin02 <95616470+JunXin02@users.noreply.github.com>
2026-06-25 14:49:49 +0600

    Update hls.js

--- api/hls.js
+++ api/hls.js
@@ -3,7 +3,7 @@ export const config = {
 };
 
 const UPSTREAM_HEADERS = {
-  Referer: "https://streamcenter.xyz/",
+  Referer: "https://streamcenter.xyz",
   Origin: "",
   "User-Agent":
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36 Edg/134.0.0.0",
